b71433c6ea12311f762734b7d65c19bfdfd40739
Aiham IT <aihamits@gmail.com>
2018-10-14 02:06:04 +0300

    create employee window

---
+++ CargoSystem/empFrm.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace CargoSystem
+{
+    partial class empFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCusMng = new System.Windows.Forms.Button();
+            this.btnCus = new System.Windows.Forms.Button();
+            this.btnTrkMng = new System.Windows.Forms.Button();
+            this.btnTrk = new System.Windows.Forms.Button();
+            this.btnDrvrRat = new System.Windows.Forms.Button();
+            this.btnDrvrMng = new System.Windows.Forms.Button();
+            this.btnDrvr = new System.Windows.Forms.Button();
+            this.btnOdrFnsh = new System.Windows.Forms.Button();
+            this.btnOdrMng = new System.Windows.Forms.Button();
+            this.btnOdr = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(173)))), ((int)(((byte)(71)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnCusMng);
+            this.panel1.Controls.Add(this.btnCus);
+            this.panel1.Controls.Add(this.btnTrkMng);
+            this.panel1.Controls.Add(this.btnTrk);
+            this.panel1.Controls.Add(this.btnDrvrRat);
+            this.panel1.Controls.Add(this.btnDrvrMng);
+            this.panel1.Controls.Add(this.btnDrvr);
+            this.panel1.Controls.Add(this.btnOdrFnsh);
+            this.panel1.Controls.Add(this.btnOdrMng);
+            this.panel1.Controls.Add(this.btnOdr);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(188, 596);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(0, 117);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 37);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnCusMng
+            // 
+            this.btnCusMng.FlatAppearance.BorderSize = 0;
+            this.btnCusMng.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCusMng.ForeColor = System.Drawing.Color.White;
+            this.btnCusMng.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCusMng.Location = new System.Drawing.Point(12, 504);
+            this.btnCusMng.Name = "btnCusMng";
+            this.btnCusMng.Size = new System.Drawing.Size(173, 37);
+            this.btnCusMng.TabIndex = 1;
+            this.btnCusMng.Text = "Manage Customers";
+            this.btnCusMng.UseVisualStyleBackColor = true;
+            this.btnCusMng.Click += new System.EventHandler(this.btnCusMng_Click);
+            // 
+            // btnCus
+            // 
+            this.btnCus.FlatAppearance.BorderSize = 0;
+            this.btnCus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCus.ForeColor = System.Drawing.Color.White;
+            this.btnCus.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCus.Location = new System.Drawing.Point(12, 461);
+            this.btnCus.Name = "btnCus";
+            this.btnCus.Size = new System.Drawing.Size(173, 37);
+            this.btnCus.TabIndex = 1;
+            this.btnCus.Text = "New Customer";
+            this.btnCus.UseVisualStyleBackColor = true;
+            this.btnCus.Click += new System.EventHandler(this.btnCus_Click);
+            // 
+            // btnTrkMng
+            // 
+            this.btnTrkMng.FlatAppearance.BorderSize = 0;
+            this.btnTrkMng.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrkMng.ForeColor = System.Drawing.Color.White;
+            this.btnTrkMng.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrkMng.Location = new System.Drawing.Point(12, 418);
+            this.btnTrkMng.Name = "btnTrkMng";
+            this.btnTrkMng.Size = new System.Drawing.Size(173, 37);
+            this.btnTrkMng.TabIndex = 1;
+            this.btnTrkMng.Text = "Manage Trucks";
+            this.btnTrkMng.UseVisualStyleBackColor = true;
+            this.btnTrkMng.Click += new System.EventHandler(this.btnTrkMng_Click);
+            // 
+            // btnTrk
+            // 
+            this.btnTrk.FlatAppearance.BorderSize = 0;
+            this.btnTrk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrk.ForeColor = System.Drawing.Color.White;
+            this.btnTrk.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrk.Location = new System.Drawing.Point(12, 375);
+            this.btnTrk.Name = "btnTrk";
+            this.btnTrk.Size = new System.Drawing.Size(173, 37);
+            this.btnTrk.TabIndex = 1;
+            this.btnTrk.Text = "New Truck";
+            this.btnTrk.UseVisualStyleBackColor = true;
+            this.btnTrk.Click += new System.EventHandler(this.btnTrk_Click);
+            // 
+            // btnDrvrRat
+            // 
+            this.btnDrvrRat.FlatAppearance.BorderSize = 0;
+            this.btnDrvrRat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDrvrRat.ForeColor = System.Drawing.Color.White;
+            this.btnDrvrRat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDrvrRat.Location = new System.Drawing.Point(12, 332);
+            this.btnDrvrRat.Name = "btnDrvrRat";
+            this.btnDrvrRat.Size = new System.Drawing.Size(173, 37);
+            this.btnDrvrRat.TabIndex = 1;
+            this.btnDrvrRat.Text = "Rate Driver";
+            this.btnDrvrRat.UseVisualStyleBackColor = true;
+            this.btnDrvrRat.Click += new System.EventHandler(this.btnDrvrRat_Click);
+            // 
+            // btnDrvrMng
+            // 
+            this.btnDrvrMng.FlatAppearance.BorderSize = 0;
+            this.btnDrvrMng.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDrvrMng.ForeColor = System.Drawing.Color.White;
+            this.btnDrvrMng.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDrvrMng.Location = new System.Drawing.Point(12, 289);
+            this.btnDrvrMng.Name = "btnDrvrMng";
+            this.btnDrvrMng.Size = new System.Drawing.Size(173, 37);
+            this.btnDrvrMng.TabIndex = 1;
+            this.btnDrvrMng.Text = "Manage Drivers";
+            this.btnDrvrMng.UseVisualStyleBackColor = true;
+            this.btnDrvrMng.Click += new System.EventHandler(this.btnDrvrMng_Click);
+            // 
+            // btnDrvr
+            // 
+            this.btnDrvr.FlatAppearance.BorderSize = 0;
+            this.btnDrvr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDrvr.ForeColor = System.Drawing.Color.White;
+            this.btnDrvr.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDrvr.Location = new System.Drawing.Point(12, 246);
+            this.btnDrvr.Name = "btnDrvr";
+            this.btnDrvr.Size = new System.Drawing.Size(173, 37);
+            this.btnDrvr.TabIndex = 1;
+            this.btnDrvr.Text = "New Driver";
+            this.btnDrvr.UseVisualStyleBackColor = true;
+            this.btnDrvr.Click += new System.EventHandler(this.btnDrvr_Click);
+            // 
+            // btnOdrFnsh
+            // 
+            this.btnOdrFnsh.FlatAppearance.BorderSize = 0;
+            this.btnOdrFnsh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOdrFnsh.ForeColor = System.Drawing.Color.White;
+            this.btnOdrFnsh.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOdrFnsh.Location = new System.Drawing.Point(12, 203);
+            this.btnOdrFnsh.Name = "btnOdrFnsh";
+            this.btnOdrFnsh.Size = new System.Drawing.Size(173, 37);
+            this.btnOdrFnsh.TabIndex = 1;
+            this.btnOdrFnsh.Text = "Complete Order";
+            this.btnOdrFnsh.UseVisualStyleBackColor = true;
+            this.btnOdrFnsh.Click += new System.EventHandler(this.btnOdrFnsh_Click);
+            // 
+            // btnOdrMng
+            // 
+            this.btnOdrMng.FlatAppearance.BorderSize = 0;
+            this.btnOdrMng.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOdrMng.ForeColor = System.Drawing.Color.White;
+            this.btnOdrMng.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOdrMng.Location = new System.Drawing.Point(12, 160);
+            this.btnOdrMng.Name = "btnOdrMng";
+            this.btnOdrMng.Size = new System.Drawing.Size(173, 37);
+            this.btnOdrMng.TabIndex = 1;
+            this.btnOdrMng.Text = "Manage Orders";
+            this.btnOdrMng.UseVisualStyleBackColor = true;
+            this.btnOdrMng.Click += new System.EventHandler(this.btnOdrMng_Click);
+            // 
+            // btnOdr
+            // 
+            this.btnOdr.FlatAppearance.BorderSize = 0;
+            this.btnOdr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOdr.ForeColor = System.Drawing.Color.White;
+            this.btnOdr.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOdr.Location = new System.Drawing.Point(12, 117);
+            this.btnOdr.Name = "btnOdr";
+            this.btnOdr.Size = new System.Drawing.Size(173, 37);
+            this.btnOdr.TabIndex = 1;
+            this.btnOdr.Tag = "Order";
+            this.btnOdr.Text = "New Order";
+            this.btnOdr.UseVisualStyleBackColor = true;
+            this.btnOdr.Click += new System.EventHandler(this.btnNewOdr_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(0, 85);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(188, 6);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(173)))), ((int)(((byte)(71)))));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Empolyeer\r\nWindow";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // empFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 596);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "empFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOdr;
+        private System.Windows.Forms.Button btnCusMng;
+        private System.Windows.Forms.Button btnCus;
+        private System.Windows.Forms.Button btnTrkMng;
+        private System.Windows.Forms.Button btnTrk;
+        private System.Windows.Forms.Button btnDrvrRat;
+        private System.Windows.Forms.Button btnDrvrMng;
+        private System.Windows.Forms.Button btnDrvr;
+        private System.Windows.Forms.Button btnOdrFnsh;
+        private System.Windows.Forms.Button btnOdrMng;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ CargoSystem/empFrm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargoSystem
+{
+    public partial class empFrm : Form
+    {
+        public empFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnNewOdr_Click(object sender, EventArgs e)
+        {
+            panel2.Top  = btnOdr.Top ; 
+        }
+
+        private void btnOdrMng_Click(object sender, EventArgs e)
+        {
+            panel2.Top  = btnOdrMng.Top ;
+        }
+
+        private void btnOdrFnsh_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnOdrFnsh.Top;
+        }
+
+        private void btnDrvr_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnDrvr.Top;
+        }
+
+        private void btnDrvrMng_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnDrvrMng.Top;
+        }
+
+        private void btnDrvrRat_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnDrvrRat.Top;
+        }
+
+        private void btnTrk_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnTrk.Top;
+        }
+
+        private void btnTrkMng_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnTrkMng.Top;
+        }
+
+        private void btnCus_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnCus.Top;
+        }
+
+        private void btnCusMng_Click(object sender, EventArgs e)
+        {
+            panel2.Top = btnCusMng.Top;
+        }
+    }
+}
